81c8bf072ed69bed281f19d586f12067c9893f4b
Yogi999-creator <yogesh.mundhra18@st.niituniversity.in>
2021-03-31 00:58:34 +0530

    Payment Gateway Razorpay

---
+++ razorpay/base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import razorpay
+from django.views.decorators.csrf import csrf_exempt
+
+
+def home(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        amount = 50000
+
+        client = razorpay.Client(
+            auth=("rzp_test_zaPI0F8nWFAb5Z", "2q4aFyWdNgmBbacfJ3fYdXyC"))
+
+        payment = client.order.create({'amount': amount, 'currency': 'INR',
+                                       'payment_capture': '1'})
+    return render(request, 'index.html')
+
+@csrf_exempt
+def success(request):
+    return render(request, "success.html")
